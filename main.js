402d4e594a728f8113f032fbdca1cef2d9d5621c
AlexanderTalyaronok <a.srgv4@gmail.com>
2021-07-17 16:01:19 +0300

    Fixed issues

--- main.js
+++ main.js
@@ -1,68 +1,33 @@
 //lesson-3
 // checks whether it's a palindrome and returns a bool value
-function palindrome(str) {
-    str = str.toLowerCase();
+const palindrome = (str) => str.toLowerCase() === str.split("").reverse().join("").toLowerCase();
 
-    return str === str.split("").reverse().join("");
-}
+console.log( palindrome("Madam") );
+console.log( palindrome("echo") );
 
- console.log( palindrome("Madam") );
- console.log( palindrome("echo") );
+// the function returns the smallest value 
+const min = (a, b) => a < b ? a : b;
 
- // the function returns the smallest value 
- function min(a, b) {
-    if (a < b) {
-        return a;
-    }
+console.log( min(6, 2) );
 
-    return b;
- }
+// the function returns the greatest value
+const max = (a, b) => a > b ? a : b;
 
- console.log( min(3, 7) );
+console.log( max(4, 9) );
 
- // the same function using the ternary operator
- function min(a, b) {
-    return a < b ? a : b;
- }
-
- console.log( min(6, 2) );
-
- // the function returns the greatest value
- function max(a, y) {
-    if (a > y) {
-        return a;
-    }
-
-    return y;
- }
-
- console.log( max(9, 4) );
-
- // the same function using the ternary operator
- function max(a, y) {
-    return a > y ? a : y;
- }
-
- console.log( max(5, 8) );
-
- // the function generates array with 10 random values between 0 and 100, then replaces 0 with "zero"
- function zeroReplace() {
+// the function generates array with 10 random values between 0 and 100, then replaces 0 with "zero"
+function zeroReplace() {
     let arr = new Array();
 
     for (let i = 0; i < 10; i++) {
-        arr[i] = Math.floor(Math.random() * 100);
+        arr[i] = Math.floor(Math.random() * 100).toString();
     }
 
-    console.log(arr);
+    let replaceFunc = (item) => item.replace(/0/g, "zero");
 
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i] === 0 || arr[i] % 10 === 0 || arr[i] === 100) {
-            arr[i] = arr[i].toString().replace("00", "zerozero");
-            arr[i] = arr[i].toString().replace("0", "zero");
-        }
-    }
+    let arrChange = arr.map(replaceFunc);
 
-    console.log(arr);
- }
+    console.log(arrChange);
+}
 
- zeroReplace();
+zeroReplace();
