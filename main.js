9fd3c0372c3a2e8ee3acff177bcf1cb1e72fd132
AlexanderTalyaronok <a.srgv4@gmail.com>
2021-07-22 17:41:32 +0300

    Added lesson-4

--- main.js
+++ main.js
@@ -30,3 +30,30 @@ const replaceZero = (arr) => arr.map((item) => String(item).includes("0") ? Stri
 
 console.log(replaceZero(generateArr(10)));
 
+// lesson-4 
+const colors = ["magenta", "cyan", "firebrick", "springgreen", "skyblue"];
+const text1 = document.getElementById("text1");
+const text2 = document.getElementById("text2");
+const text3 = document.getElementById("text3");
+
+function changeColor(textId) {
+    for (let i = 0; i < colors.length; i++) {
+        if (i === colors.length - 1) {
+            return textId.style.backgroundColor = colors[0];
+        }
+        else if (textId.style.backgroundColor === colors[i]) {
+            return textId.style.backgroundColor = colors[++i];
+        } 
+    }
+
+    return textId.style.backgroundColor = colors[0];
+}
+
+text1.addEventListener('click', changeColor.bind(null, text1));
+text2.addEventListener('click', changeColor.bind(null, text2));
+text3.addEventListener('click', changeColor.bind(null, text3));
+
+// the sum function 
+const sum = (a) => (b) => a + b;
+
+console.log(sum(5)(2));
