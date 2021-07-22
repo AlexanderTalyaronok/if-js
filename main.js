93837c0a4cf791c42207456c812ca9eff244b5aa
AlexanderTalyaronok <a.srgv4@gmail.com>
2021-07-22 14:58:39 +0300

    Works as intended

--- main.js
+++ main.js
@@ -16,16 +16,23 @@ const max = (a, b) => a > b ? a : b;
 console.log(max(4, 9));
 
 // the function generates array with 10 random values between 0 and 100, then replaces 0 with "zero"
-const arr = [];
-
 const generateArr = (arrLength) => {
+    const arr = [];
+    
     for (let i = 0; i < arrLength; i++) {
-        arr[i] = Math.floor(Math.random() * 100).toString();
+        arr[i] = Math.floor(Math.random() * 100);
     }
 
     return arr;
 };
  
-const replaceZero = () => arr.map(item => item.replace(/0/g, "zero"));
+const replaceZero = (arr) => arr.map((item) => {
+    if (item % 10 === 0) {
+        return String(item).replace(/0/g, "zero");
+    }
+
+    return item;
+});
 
 console.log(replaceZero(generateArr(10)));
+
