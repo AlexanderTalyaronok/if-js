148fb884c39ed3090dbef43679b7c4812828bace
AlexanderTalyaronok <a.srgv4@gmail.com>
2021-08-05 23:14:00 +0300

    lesson-6

---
+++ js/lesson-6.js
@@ -0,0 +1,54 @@
+// Date conversion according to the specified format
+const date = '2020-11-26';
+const formatDate = date.split('-').reverse().join('.');
+
+console.log(formatDate);
+
+// The search function
+const data = [
+    {
+      country: 'Russia',
+      city: 'Saint Petersburg',
+      hotel: 'Hotel Leopold',
+    },
+    {
+      country: 'Spain',
+      city: 'Santa Cruz de Tenerife',
+      hotel: 'Apartment Sunshine',
+    },
+    {
+      country: 'Slowakia',
+      city: 'Vysokie Tatry',
+      hotel: 'Villa Kunerad',
+    },
+    {
+      country: 'Germany',
+      city: 'Berlin',
+      hotel: 'Hostel Friendship',
+    },
+    {
+      country: 'Indonesia',
+      city: 'Bali',
+      hotel: 'Ubud Bali Resort&SPA',
+    },
+    {
+      country: 'Netherlands',
+      city: 'Rotterdam',
+      hotel: 'King Kong Hostel',
+    },
+    {
+      country: 'Marocco',
+      city: 'Ourika',
+      hotel: 'Rokoko Hotel',
+    },
+    {
+      country: 'Germany',
+      city: 'Berlin',
+      hotel: 'Hotel Rehberge Berlin Mitte',
+    },
+  ];
+
+  const searchInfo = (str) => data.filter((item) => Object.values(item).includes(str));
+
+  console.log(searchInfo('King Kong Hostel'));
+
