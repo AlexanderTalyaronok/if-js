de4d9d76d0125979b1cc1fd02f7f509b5f10d6f9
AlexanderTalyaronok <a.srgv4@gmail.com>
2021-08-28 15:24:36 +0300

    lesson-9

---
+++ js/lesson-9.js
@@ -0,0 +1,67 @@
+const studentsData = [
+  {
+    firstName: 'Василий',
+    lastName: 'Петров',
+    admissionYear: 2019,
+    courseName: 'Java',
+  },
+  {
+    firstName: 'Иван',
+    lastName: 'Иванов',
+    admissionYear: 2018,
+    courseName: 'JavaScript',
+  },
+  {
+    firstName: 'Александр',
+    lastName: 'Федоров',
+    admissionYear: 2017,
+    courseName: 'Python',
+  },
+  {
+    firstName: 'Николай',
+    lastName: 'Петров',
+    admissionYear: 2019,
+    courseName: 'Android',
+  }
+];
+
+class User {
+  constructor(firstName, lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  get fullName() {
+    return `${this.firstName} ${this.lastName}`;
+  }
+}
+
+class Student extends User {
+  constructor(firstName, lastName, admissionYear, courseName) {
+    super(firstName, lastName);
+    this.admissionYear = admissionYear;
+    this.courseName = courseName;
+  }
+
+  get course() {
+    return new Date().getFullYear() - this.admissionYear;
+  }
+}
+
+class Students {
+  constructor(studentsData) {
+    this.studentsData = studentsData;
+  }
+
+  getInfo() {
+    return this.studentsData.sort((a, b) => b.admissionYear - a.admissionYear).reduce((acc, curr) => {
+      const student = new Student(curr.firstName, curr.lastName, curr.admissionYear, curr.courseName);
+      acc.push(`${student.fullName} - ${student.courseName}, ${student.course} курс`);
+      return acc;
+    }, [])
+  }
+}
+
+const student = new Students(studentsData);
+
+console.log(student.getInfo());
